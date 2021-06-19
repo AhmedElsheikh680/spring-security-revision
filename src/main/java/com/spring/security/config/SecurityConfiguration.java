bc8213030dc00cfc23983bdad651771daf3fec50
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-19 12:36:03 +0200

    SecurityConfiguration and add user inMemoryAuthentication() Closes #3

---
+++ src/main/java/com/spring/security/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+             .inMemoryAuthentication()
+             .withUser("ahmed").password(passwordEncoder().encode("ahmed123")).roles("ADMIN")
+             .and()
+             .withUser("kareem").password(passwordEncoder().encode("kareem123")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}

799be3c3123fec180c7ed7ffad9bdf53daef0ff0
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-19 22:01:51 +0200

    add manager and user Closes #11

--- src/main/java/com/spring/security/bootstrap/DBInit.java
+++ src/main/java/com/spring/security/bootstrap/DBInit.java
@@ -1,5 +1,7 @@
 package com.spring.security.bootstrap;
 
+import com.spring.security.model.Authorities;
+import com.spring.security.model.Role;
 import com.spring.security.model.User;
 import com.spring.security.repo.AuthoritiesRepo;
 import com.spring.security.repo.RoleRepo;
@@ -26,9 +28,29 @@ public class DBInit  implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User("ahmed.elsheikh@yahoo.com", "ahmed123", "25", "Cairo", 1);
-        user.setRoles(roleRepo.findAll());
-        user.setAuthorities(authoritiesRepo.findAll());
+        userRepo.deleteAll();
+        User admin = new User("ahmed.elsheikh@yahoo.com", "ahmed123", "25", "Cairo", 1);
+        admin.setRoles(roleRepo.findAll());
+        admin.setAuthorities(authoritiesRepo.findAll());
+        userRepo.save(admin);
+        ////////////////////////////////////////////////
+        User manager = new User("yasser.ali@yahoo.com", "yasser123", "40", "Tanata", 1);
+        Role managerRole1 =  roleRepo.findById(2L).get();
+        Role managerRole2 = roleRepo.findById(3L).get();
+        Authorities managerAuthorities1 = authoritiesRepo.findById(2L).get();
+        Authorities managerAuthorities2 = authoritiesRepo.findById(3L).get();
+
+        manager.getRoles().add(managerRole1);
+        manager.getRoles().add(managerRole2);
+        manager.getAuthorities().add(managerAuthorities1);
+        manager.getAuthorities().add(managerAuthorities2);
+        userRepo.save(manager);
+        ///////////////////////////////////////////////
+        User user = new User("kareem.ayman@yahoo.com", "kareem123", "50", "Giza", 1);
+        Role userRole = roleRepo.findById(3L).get();
+        Authorities userAuthorities = authoritiesRepo.findById(3L).get();
+        user.getRoles().add(userRole);
+        user.getAuthorities().add(userAuthorities);
         userRepo.save(user);
 
     }

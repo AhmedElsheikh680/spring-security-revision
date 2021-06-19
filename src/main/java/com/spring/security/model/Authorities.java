94e1fb793400defdef8173fdf8f24453c19c7512
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-19 18:00:26 +0200

    add annotation model classes Closes #8

--- src/main/java/com/spring/security/model/Authorities.java
+++ src/main/java/com/spring/security/model/Authorities.java
@@ -1,12 +1,20 @@
 package com.spring.security.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "authorities")
 public class Authorities {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "authoritize")
     private String authorize;
 
+    @ManyToMany(mappedBy = "authorities")
     private Set<User> users;
 }
--- src/main/java/com/spring/security/model/Role.java
+++ src/main/java/com/spring/security/model/Role.java
@@ -1,12 +1,20 @@
 package com.spring.security.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "role")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "role_name")
     private String roleName;
 
+    @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 }

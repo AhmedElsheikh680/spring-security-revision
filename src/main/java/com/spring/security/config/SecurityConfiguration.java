5868606ca2f9df1fb48cc594f4fd23603c4d0185
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-20 00:08:49 +0200

    handle SecurityConfiguration Closes #13

--- src/main/java/com/spring/security/config/SecurityConfiguration.java
+++ src/main/java/com/spring/security/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,22 +14,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private UserPricipleDetailsService userPricipleDetailsService;
+    public SecurityConfiguration(UserPricipleDetailsService userPricipleDetailsService){
+        this.userPricipleDetailsService = userPricipleDetailsService;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-             .inMemoryAuthentication()
-             .withUser("ahmed")
-             .password(passwordEncoder().encode("ahmed123"))
-//             .roles("ADMIN")
-             .authorities("ACCESS_BASIC1", "ROLE_ADMIN")
-             .and()
-             .withUser("yasser")
-             .password(passwordEncoder()
-             .encode("yasser123"))
-//             .roles("MANAGER")
-             .authorities("ACCESS_BASIC2", "ROLE_MANAGER")
-             .and()
-             .withUser("kareem").password(passwordEncoder().encode("kareem123")).roles("USER");
+        auth.
+                authenticationProvider(authenticationProvider());
+
+//        auth
+//             .inMemoryAuthentication()
+//             .withUser("ahmed")
+//             .password(passwordEncoder().encode("ahmed123"))
+////             .roles("ADMIN")
+//             .authorities("ACCESS_BASIC1", "ROLE_ADMIN")
+//             .and()
+//             .withUser("yasser")
+//             .password(passwordEncoder()
+//             .encode("yasser123"))
+////             .roles("MANAGER")
+//             .authorities("ACCESS_BASIC2", "ROLE_MANAGER")
+//             .and()
+//             .withUser("kareem").password(passwordEncoder().encode("kareem123")).roles("USER");
     }
 
     @Override
@@ -44,6 +53,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/basic/allbasic").hasAuthority("ACCESS_BASIC2")
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userPricipleDetailsService);
+        return daoAuthenticationProvider;
     }
 
     @Bean
